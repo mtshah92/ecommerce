722217a0ba7a6b83ffc44a53e11ec837fc568840
mtshah92 <mtshah92@gmail.com>
2023-07-26 15:50:38 +0530

    refactor

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -4,8 +4,8 @@ import { NavBar } from "../../components/navigation/navigation";
 
 export const Profile = () => {
   const { logoutHandler, authState, user } = useContext(AuthContext);
-  // console.log(user);
-
+  console.log(user);
+  console.log("any");
   return (
     <div>
       <NavBar />
